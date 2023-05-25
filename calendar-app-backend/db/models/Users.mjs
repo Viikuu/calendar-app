228b96d6aafe5804dd38d76db72f4268b373ce95
Viikuu <viiktorw77@gmail.com>
2023-05-26 00:11:48 +0200

    feat: create user mongoose schema

---
+++ calendar-app-backend/db/models/Users.mjs
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 50,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+});
+
+export const UserModel = mongoose.model('User', userSchema);
