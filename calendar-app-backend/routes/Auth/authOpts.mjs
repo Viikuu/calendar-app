48260e525256e59225c06546484303f279d10644
Viikuu <viiktorw77@gmail.com>
2023-06-01 15:52:33 +0200

    feat: add serverside validation

--- calendar-app-backend/routes/Auth/authOpts.mjs
+++ calendar-app-backend/routes/Auth/authOpts.mjs
@@ -1,3 +1,70 @@
-export const createUserOpts = {};
-export const authUserOpts = {};
-export const getUserOpts = {};
+const userSchema = {
+  type: 'object',
+  properties: {
+    _id: { type: 'string' },
+    email: { type: 'string' },
+  },
+};
+
+export const createUserOpts = {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['email', 'password'],
+      properties: {
+        email: {
+          type: 'string',
+        },
+        password: {
+          type: 'string',
+        },
+      },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          user: userSchema,
+        },
+      },
+    },
+  },
+};
+
+export const authUserOpts = {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['email', 'password'],
+      properties: {
+        email: {
+          type: 'string',
+        },
+        password: {
+          type: 'string',
+        },
+      },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          user: userSchema,
+        },
+      },
+    },
+  },
+};
+
+export const getUserOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          user: userSchema,
+        },
+      },
+    },
+  },
+};
