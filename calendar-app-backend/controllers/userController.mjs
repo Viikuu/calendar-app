619f486f737f31ade333451481f210360e4e3b3b
Viikuu <viiktorw77@gmail.com>
2023-06-06 21:27:18 +0200

    feat: create update userData endpoint
    create update func for users country, city, showWeather, showHolidays variables

--- calendar-app-backend/controllers/userController.mjs
+++ calendar-app-backend/controllers/userController.mjs
@@ -1,15 +1,35 @@
-import { UserModel } from "../db/models/Users.mjs";
+import { CountryModel } from '../db/models/Country.mjs';
+import { UserModel } from '../db/models/Users.mjs';
 
 export async function updateUser(request, reply) {
   try {
-    const userData = request.body;
+    const { city, country, showHolidays, showWeather } = request.body;
+    let parsedUserData = {};
+    if (city) {
+      parsedUserData['city'] = city;
+    }
+
+    if (country) {
+      const { code } = await CountryModel.find({ name: country });
+      parsedUserData['countryCode'] = code;
+      parsedUserData['country'] = country;
+    }
+
+    if (showHolidays) {
+      parsedUserData['showHolidays'] = showHolidays;
+    }
+
+    if (showWeather) {
+      parsedUserData['showWeather'] = showWeather;
+    }
     const { _doc: existingUser } = await UserModel.findByIdAndUpdate(
       { _id: request.user._id },
-      userData,
+      parsedUserData,
       { new: true },
     ).exec();
     return existingUser;
   } catch (error) {
+    )
     throw new Error(`Something went wrong! Try again`);
   }
 }
--- calendar-app-backend/routes/User/userRouter.mjs
+++ calendar-app-backend/routes/User/userRouter.mjs
@@ -2,6 +2,15 @@ import { updateUser } from '../../controllers/userController.mjs';
 
 export const userRouter = async (fastify, opts, done) => {
   await fastify.put('/', async function setLoc(request, reply) {
+    const { city } = request.body;
+    if (city) {
+      const geoLoc = await (
+        await fetch(`https://geocode.maps.co/search?q={${city}}`)
+      ).json();
+      if (geoLoc.length === 0) {
+        fastify.httpErrors.badRequest('Invalid city name');
+      }
+    }
     const { password, ...userData } = await updateUser(request, reply);
 
     return {
