d419dbc919ea1acf72b0b7ca86dfb6fbe4777c4a
Viikuu <viiktorw77@gmail.com>
2023-06-08 23:24:48 +0200

    feat: create new and fix old types

--- calendar-app-front/src/utils/types.ts
+++ calendar-app-front/src/utils/types.ts
@@ -4,6 +4,8 @@ export interface DayEventI {
   date: Date,
   title: string,
   description: string,
+  type: string,
+  location: string,
 }
 
 export interface CalendarDate {
@@ -12,14 +14,28 @@ export interface CalendarDate {
   year: number;
   weekday: Weekday;
   active: boolean;
-  events: Array<DayEventI>
+  events: Array<DayEventI>;
+  weatherEvents: DayEventI | null;
+  holidayEvents: Array<DayEventI>;
 }
 
 export type userData = {
-  user: {
-    _id: string,
-    email: string,
-  }
+  user: userType,
+}
+
+export type userType = {
+  _id: string,
+  email: string,
+  city: string,
+  country: string,
+  showHolidays: boolean,
+  showWeather: boolean,
+
+}
+
+export type countriesType = {
+  name: string,
+  code: string,
 }
 
 export type Weekday = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
