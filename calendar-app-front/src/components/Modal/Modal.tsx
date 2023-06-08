96a229dc7c3cb66516847346d4c55633fcde95f0
Viikuu <viiktorw77@gmail.com>
2023-06-08 11:34:35 +0200

    feat: update modal design and functionality

--- calendar-app-front/src/components/Modal/Modal.tsx
+++ calendar-app-front/src/components/Modal/Modal.tsx
@@ -9,14 +9,18 @@ export const Modal: React.FC<ModalProps> = (props) => {
 
   return <>
     {props.show ? <div className="modal">
+      
       <div className="modal-content">
-      {props.children}
-    
-    <button className="closeModal xButton" onClick={() => {
-            props.setShow(false);
-         }}
-        > X </button>
+        <div className="bar">
+          <button className="closeModal xButton" onClick={() => {
+              props.setShow(false);
+          }}
+          > X </button>
         </div>
+        <div>
+          {props.children}
+        </div>
+      </div>
     </div> : null}
     </>;
   
