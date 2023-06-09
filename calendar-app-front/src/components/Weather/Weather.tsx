f1b51ab64ccf2c29510923116ac223be4cea5a1c
Viikuu <viiktorw77@gmail.com>
2023-06-09 10:54:57 +0200

    fix: fix weather icon displaying

--- calendar-app-front/src/components/Weather/Weather.tsx
+++ calendar-app-front/src/components/Weather/Weather.tsx
@@ -1,40 +1,19 @@
 import React from 'react';
-import brokenClouds from './img/brokenClouds.png'
-import clearSky from './img/clearSky.png'
-import fewClouds from './img/fewClouds.png'
-import mist from './img/mist.png'
-import rain from './img/rain.png'
-import scatteredClouds from './img/scatteredClouds.png'
-import showerRain from './img/showerRain.png'
-import snow from './img/snow.png'
-import thunderstorm from './img/thunderstorm.png'
+import './Weather.css'
 
 export const Weather: React.FC<{ temperature: string, icon: string }> = ({temperature, icon}) => {
   return (
       <div className="weather">
-        <img className="weatherImage" src={(() => {
-          switch(icon) {
-            case 'broken clouds':
-              return brokenClouds;
-            case 'clear sky':
-              return clearSky;
-            case 'few clouds':
-              return fewClouds;
-            case 'mist':
-              return mist;
-            case 'rain':
-              return rain;
-            case 'scattered clouds':
-              return scatteredClouds;
-            case 'shower rain':
-              return showerRain;
-            case 'snow':
-              return snow;
-            case 'thunderstorm':
-              return thunderstorm;
-            default:
-              return ''
-          }
+      <img className="weatherImage" src={(() => {
+        if(['803', '804'].includes(icon)) return 'http://openweathermap.org/img/wn/04d@2x.png';
+        if(['800'].includes(icon)) return 'http://openweathermap.org/img/wn/01d@2x.png';
+        if(['801'].includes(icon)) return 'http://openweathermap.org/img/wn/02d@2x.png';
+        if(['701', '711', '721', '731', '741', '751', '761', '762', '771', '781'].includes(icon)) return 'http://openweathermap.org/img/wn/50d@2x.png';
+        if(['500', '501', '502', '503', '504'].includes(icon)) return 'http://openweathermap.org/img/wn/10d@2x.png';
+        if(['802'].includes(icon)) return 'http://openweathermap.org/img/wn/03d@2x.png';
+        if(['520', '521', '522', '531', '300', '301', '302', '310', '311', '312', '313', '314', '321'].includes(icon))return 'http://openweathermap.org/img/wn/09d@2x.png';
+        if(['511', '600', '601', '602', '611', '612', '613', '615', '616', '620', '621', '622'].includes(icon)) return 'http://openweathermap.org/img/wn/13d@2x.png';
+        if(['200', '201', '202', '210', '211', '212', '221', '230', '231', '232'].includes(icon)) return 'http://openweathermap.org/img/wn/11d@2x.png';
       })()}></img>
         <div className="temperature">
           {temperature}
